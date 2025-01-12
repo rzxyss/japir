beae5173c9b1b4451afb7ede63a8d53f8c0469cc
Rizki Saepul Aziz <rzkysa0@gmail.com>
2025-01-12 22:57:30 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\MobilController;
 use App\Http\Controllers\SupirController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +12,8 @@ Route::get('/', [AuthenticatedSessionController::class, 'create'])->middleware('
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('/supir', SupirController::class);
+    Route::resource('/mobil', MobilController::class);
+    Route::resource('/jadwal', JadwalController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
